4d636eb2a4ce69e9c9f4aaa0a2286a756c156b22
quliyevadinara <quliyevadinara14@gmail.com>
2026-05-24 18:54:45 +0400

    task 5 and 8 fixed

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:02d}".format(i), end="")
-    if i < 99:
-        print(", ", end="")
+    print("{:02d}{:s}".format(i, ", " if i < 99 else ""), end="")
 print("")
