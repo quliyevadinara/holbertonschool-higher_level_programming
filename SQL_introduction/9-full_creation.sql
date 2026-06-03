ebf34dd6cd9651e7dc928885a29a139e5e27dbb1
quliyevadinara <quliyevadinara14@gmail.com>
2026-06-03 22:16:31 +0400

    fix

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,18 @@
---Full creation
+-- Create table second_table if it does not exist
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
+-- Insert John
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 
+-- Insert Alex
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 
+-- Insert Bob
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 
+-- Insert George
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
