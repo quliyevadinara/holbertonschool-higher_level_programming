48a1ccc8df15c9f866af85c71955185015c9e45b
quliyevadinara <quliyevadinara14@gmail.com>
2026-06-03 23:02:11 +0400

    all tasks completed

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Creates table unique_id with id defaulting to 1 and must be unique
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- Lists all cities of California using a subquery (no JOIN)
+SELECT id, name
+FROM cities
+WHERE
+    state_id = (
+        SELECT id
+        FROM states
+        WHERE
+            name = 'California'
+    )
+ORDER BY id ASC;
