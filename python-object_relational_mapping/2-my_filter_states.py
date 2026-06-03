62c901a44622a61de53d6674c23ea2fc0155881c
quliyevadinara <quliyevadinara14@gmail.com>
2026-06-03 23:51:24 +0400

    Fix case-sensitive search using BINARY keyword

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,10 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
     cursor.execute(query.format(sys.argv[4]))
     for row in cursor.fetchall():
         print(row)
     cursor.close()
     db.close()
+    
